d14ec8209d1a8611cd499c135836a613afc5ff32
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-11-23 23:46:07 +0100

    ui: add launcher activity for testing

--- app/src/main/java/org/faudroids/loooooading/ui/MainActivity.java
+++ app/src/main/java/org/faudroids/loooooading/ui/MainActivity.java
@@ -1,172 +1,36 @@
 package org.faudroids.loooooading.ui;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PixelFormat;
-import android.graphics.PorterDuff;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 import org.faudroids.loooooading.R;
-import org.faudroids.loooooading.game.GameManager;
-import org.faudroids.loooooading.game.Player;
-import org.faudroids.loooooading.game.Snowflake;
-import org.roboguice.shaded.goole.common.base.Optional;
-
-import javax.inject.Inject;
 
 import roboguice.activity.RoboActionBarActivity;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
-import timber.log.Timber;
 
 @ContentView(R.layout.activity_main)
-public class MainActivity extends RoboActionBarActivity implements SurfaceHolder.Callback {
+public class MainActivity extends RoboActionBarActivity {
 
-
-	@InjectView(R.id.surface_view) private SurfaceView surfaceView;
-    @InjectView(R.id.xCoord) private TextView xCoordTV;
-    @InjectView(R.id.yCoord) private TextView yCoordTV;
-
-	@Inject private GameManager gameManager;
-
-	private Optional<DrawSnowflakesRunnable> drawRunnable = Optional.absent();
-	private Optional<SurfaceHolder> surfaceHolder = Optional.absent();
-
+	@InjectView(R.id.btn_swipe) private Button swipeButton;
+	@InjectView(R.id.btn_game) private Button gameButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		SurfaceHolder surfaceHolder = surfaceView.getHolder();
-		surfaceHolder.addCallback(this);
-		surfaceHolder.setFormat(PixelFormat.TRANSPARENT);
-		surfaceView.setBackgroundColor(Color.TRANSPARENT);
-		surfaceView.setZOrderOnTop(true);
-
-	}
-
-
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		surfaceHolder = Optional.of(holder);
-		startSnowflakes();
-
-        surfaceView.setOnTouchListener(new View.OnTouchListener() {
+		swipeButton.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-
-				//return super.onTouchEvent(event);
-				int action = event.getAction();
-
-				switch (action) {
-					case MotionEvent.ACTION_UP:
-					case MotionEvent.ACTION_MOVE:
-					case MotionEvent.ACTION_DOWN:
-
-						xCoordTV.setText("x: " + event.getX());
-						yCoordTV.setText("y: " + event.getY());
-						gameManager.onPlayerTouch(event.getX());
-						break;
-
-					default:
-						break;
-				}
-
-				return true;
-
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, SwipeRefreshActivity.class));
+			}
+		});
+		gameButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, GameTestActivity.class));
 			}
 		});
 	}
-
-
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-		// nothing to do for now
-	}
-
-
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		surfaceHolder = Optional.absent();
-		stopSnowflakes();
-	}
-
-
-	private void startSnowflakes() {
-		if (surfaceHolder.isPresent()) {
-			drawRunnable = Optional.of(new DrawSnowflakesRunnable(surfaceHolder.get()));
-			new Thread(drawRunnable.get()).start();
-		}
-	}
-
-
-	private void stopSnowflakes() {
-		if (drawRunnable.isPresent()) {
-			drawRunnable.get().stop();
-			drawRunnable = Optional.absent();
-		}
-
-	}
-
-	private class DrawSnowflakesRunnable implements Runnable {
-
-		private final int msPerFrame = 1000 / 50; // = 40 FPS
-		private final Paint PAINT = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG | Paint.DITHER_FLAG);
-
-		private final SurfaceHolder surfaceHolder;
-		private volatile boolean isRunning = true;
-
-
-		public DrawSnowflakesRunnable(SurfaceHolder surfaceHolder) {
-			this.surfaceHolder = surfaceHolder;
-		}
-
-		@Override
-		public void run() {
-			Canvas tmpCanvas = surfaceHolder.lockCanvas();
-			gameManager.start(tmpCanvas.getWidth(), tmpCanvas.getHeight());
-			surfaceHolder.unlockCanvasAndPost(tmpCanvas);
-
-			while (isRunning) {
-				final Canvas canvas = surfaceHolder.lockCanvas();
-				canvas.drawColor(0, PorterDuff.Mode.CLEAR);
-
-				// draw player
-				Player player = gameManager.getPlayer();
-				canvas.drawBitmap(player.getDefaultBitmap(), player.getMatrix(), PAINT);
-
-
-				// draw snowflakes
-				for (Snowflake snowflake : gameManager.getSnowflakes()) {
-					canvas.drawBitmap(snowflake.getBitmap(), snowflake.getMatrix(), PAINT);
-				}
-
-				surfaceHolder.unlockCanvasAndPost(canvas);
-
-				// update game
-				long timeDiff = gameManager.loop();
-
-				try {
-
-					long sleepTime = Math.max(0, msPerFrame + (msPerFrame - timeDiff));
-					Thread.sleep(sleepTime);
-				} catch (InterruptedException e) {
-					Timber.e(e, "interrupted while sleeping in draw");
-				}
-			}
-		}
-
-		public void stop() {
-			isRunning = false;
-		}
-
-	}
-
-
 }
