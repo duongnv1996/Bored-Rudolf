b37dcda857cfd9812004b47c05885b40a755420f
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-12-04 21:46:55 +0100

    ui: add ListView to SwipeRefreshActivity

--- app/src/main/java/org/faudroids/loooooading/ui/SwipeRefreshActivity.java
+++ app/src/main/java/org/faudroids/loooooading/ui/SwipeRefreshActivity.java
@@ -1,6 +1,8 @@
 package org.faudroids.loooooading.ui;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.faudroids.loooooading.R;
 
@@ -12,13 +14,16 @@ import roboguice.inject.InjectView;
 public class SwipeRefreshActivity extends RoboActionBarActivity implements CustomSwipeRefreshLayout.OnRefreshListener {
 
     @InjectView(R.id.layout_swipe) private CustomSwipeRefreshLayout refreshLayout;
+	@InjectView(R.id.list_view) private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         refreshLayout.setOnRefreshListener(this);
-		// refreshLayout.setCustomHeadview(new MySwipeRefreshHeadView(this));
+		ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+		listView.setAdapter(listAdapter);
+		listAdapter.addAll("pull", "down", "to", "refresh", "this", "layout", "!", "la", "la", "la", "la", "la", "...");
     }
 
     @Override
