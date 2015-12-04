939aa4b1be034f658d89109e6c3394703e44b428
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-12-04 20:52:29 +0100

    ui: add DI to SwipeRefreshActivity

--- app/src/main/java/org/faudroids/loooooading/ui/SwipeRefreshActivity.java
+++ app/src/main/java/org/faudroids/loooooading/ui/SwipeRefreshActivity.java
@@ -1,55 +1,37 @@
 package org.faudroids.loooooading.ui;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import org.faudroids.loooooading.R;
 
-public class SwipeRefreshActivity extends AppCompatActivity implements CustomSwipeRefreshLayout.OnRefreshListener {
+import roboguice.activity.RoboActionBarActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
-    CustomSwipeRefreshLayout mCSRL;
+@ContentView(R.layout.activity_swipe_refresh)
+public class SwipeRefreshActivity extends RoboActionBarActivity implements CustomSwipeRefreshLayout.OnRefreshListener {
+
+    @InjectView(R.id.swipelayout) private CustomSwipeRefreshLayout refreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_swipe_refresh);
-        mCSRL = (CustomSwipeRefreshLayout) findViewById(R.id.swipelayout);
-        mCSRL.setOnRefreshListener(this);
+
+        refreshLayout.setOnRefreshListener(this);
+		refreshLayout.setCustomHeadview(new MySwipeRefreshHeadView(this));
 
         // this is anyway set as default
         // mCSRL.setCustomHeadview(new DefaultCustomHeadView(this));
 
-        mCSRL.setCustomHeadview(new MySwipeRefreshHeadView(this));
-
-        // let's say refresh task takes 10secs
-        AsyncTask t = new AsyncTask() {
-            @Override
-            protected Object doInBackground(Object[] params) {
-                synchronized(this) {
-                    try {
-                        wait(1000 * 10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Object o) {
-                super.onPostExecute(o);
-                mCSRL.refreshComplete();
-            }
-        };
-        t.execute();
-
-        // when refreshing done call
-        // mCSRL.refreshComplete();
     }
 
     @Override
     public void onRefresh() {
-        Toast.makeText(this, "Refreshing...", Toast.LENGTH_LONG).show();
+		refreshLayout.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				refreshLayout.refreshComplete();
+			}
+		}, 2000);
     }
 }
