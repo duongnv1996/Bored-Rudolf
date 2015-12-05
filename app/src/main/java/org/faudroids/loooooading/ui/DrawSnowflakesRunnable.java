c73ed84b5809764023fa5cf78403ac822445bf09
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-12-05 18:24:34 +0100

    ui: move DrawSnowflakesRunnable to seperate class

---
+++ app/src/main/java/org/faudroids/loooooading/ui/DrawSnowflakesRunnable.java
@@ -0,0 +1,112 @@
+package org.faudroids.loooooading.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.view.SurfaceHolder;
+
+import org.faudroids.loooooading.game.GameManager;
+import org.faudroids.loooooading.game.Player;
+import org.faudroids.loooooading.game.Snowflake;
+
+import timber.log.Timber;
+
+
+class DrawSnowflakesRunnable implements Runnable {
+
+	private static final boolean DEBUG = false;
+
+	private static final int MS_PER_FRAME = 1000 / 50; // = 40 FPS
+
+	private static final int CHEWING_ANIM_FRAME_LENGTH_IN_MS = 150;
+
+	private final Paint
+			PAINT = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG | Paint.DITHER_FLAG),
+			DEBUG_PAINT = new Paint(),
+			DEBUG_PAINT_STROKE = new Paint();
+
+	private final Context context;
+	private final GameManager gameManager;
+	private final SurfaceHolder surfaceHolder;
+
+	private volatile boolean isRunning = true;
+
+
+	public DrawSnowflakesRunnable(Context context, GameManager gameManager, SurfaceHolder surfaceHolder) {
+		this.context = context;
+		this.gameManager = gameManager;
+		this.surfaceHolder = surfaceHolder;
+
+		DEBUG_PAINT.setColor(context.getResources().getColor(android.R.color.holo_red_light));
+		DEBUG_PAINT_STROKE.setColor(context.getResources().getColor(android.R.color.holo_red_light));
+		DEBUG_PAINT_STROKE.setStrokeWidth(3);
+		DEBUG_PAINT_STROKE.setStyle(Paint.Style.STROKE);
+	}
+
+	@Override
+	public void run() {
+		Canvas tmpCanvas = surfaceHolder.lockCanvas();
+		gameManager.start(tmpCanvas.getWidth(), tmpCanvas.getHeight());
+		surfaceHolder.unlockCanvasAndPost(tmpCanvas);
+
+		while (isRunning) {
+			final Canvas canvas = surfaceHolder.lockCanvas();
+			canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+
+			// draw player
+			Player player = gameManager.getPlayer();
+			switch (player.getState()) {
+				case DEFAULT:
+					canvas.drawBitmap(player.getDefaultBitmap(), player.getMatrix(), PAINT);
+					break;
+
+				case LOOKING_UP:
+					canvas.drawBitmap(player.getLookingUpBitmap(), player.getMatrix(), PAINT);
+					break;
+
+				case CHEWING:
+					if ((player.getChewingDuration() / CHEWING_ANIM_FRAME_LENGTH_IN_MS) % 2 == 0) {
+						canvas.drawBitmap(player.getChewing0Bitmap(), player.getMatrix(), PAINT);
+					} else {
+						canvas.drawBitmap(player.getChewing1Bitmap(), player.getMatrix(), PAINT);
+					}
+					break;
+			}
+
+
+			// draw snowflakes
+			for (Snowflake snowflake : gameManager.getSnowflakes()) {
+				canvas.drawBitmap(snowflake.getBitmap(), snowflake.getMatrix(), PAINT);
+			}
+
+			// draw debug
+			if (DEBUG) {
+				canvas.drawRect(player.getMouthRect(), DEBUG_PAINT_STROKE);
+				canvas.drawCircle(player.getxPos(), player.getyPos(), 3, DEBUG_PAINT);
+				for (Snowflake snowflake : gameManager.getSnowflakes()) {
+					canvas.drawRect(snowflake.getBoundingBox(), DEBUG_PAINT_STROKE);
+					canvas.drawCircle(snowflake.getCenter().x, snowflake.getCenter().y, 3, DEBUG_PAINT);
+				}
+			}
+
+			surfaceHolder.unlockCanvasAndPost(canvas);
+
+			// update game
+			long timeDiff = gameManager.loop();
+
+			try {
+
+				long sleepTime = Math.max(0, MS_PER_FRAME + (MS_PER_FRAME - timeDiff));
+				Thread.sleep(sleepTime);
+			} catch (InterruptedException e) {
+				Timber.e(e, "interrupted while sleeping in draw");
+			}
+		}
+	}
+
+	public void stop() {
+		isRunning = false;
+	}
+
+}
--- app/src/main/java/org/faudroids/loooooading/ui/GameTestActivity.java
+++ app/src/main/java/org/faudroids/loooooading/ui/GameTestActivity.java
@@ -1,10 +1,7 @@
 package org.faudroids.loooooading.ui;
 
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.PixelFormat;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -14,8 +11,6 @@ import android.widget.TextView;
 
 import org.faudroids.loooooading.R;
 import org.faudroids.loooooading.game.GameManager;
-import org.faudroids.loooooading.game.Player;
-import org.faudroids.loooooading.game.Snowflake;
 import org.roboguice.shaded.goole.common.base.Optional;
 
 import javax.inject.Inject;
@@ -23,13 +18,9 @@ import javax.inject.Inject;
 import roboguice.activity.RoboActionBarActivity;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
-import timber.log.Timber;
 
 @ContentView(R.layout.activity_game_test)
 public class GameTestActivity extends RoboActionBarActivity implements SurfaceHolder.Callback {
-
-	private static final boolean DEBUG = false;
-
 
 	@InjectView(R.id.surface_view) private SurfaceView surfaceView;
     @InjectView(R.id.xCoord) private TextView xCoordTV;
@@ -109,7 +100,7 @@ public class GameTestActivity extends RoboActionBarActivity implements SurfaceHo
 
 	private void startSnowflakes() {
 		if (surfaceHolder.isPresent()) {
-			drawRunnable = Optional.of(new DrawSnowflakesRunnable(surfaceHolder.get()));
+			drawRunnable = Optional.of(new DrawSnowflakesRunnable(this, gameManager, surfaceHolder.get()));
 			new Thread(drawRunnable.get()).start();
 		}
 	}
@@ -119,98 +110,6 @@ public class GameTestActivity extends RoboActionBarActivity implements SurfaceHo
 		if (drawRunnable.isPresent()) {
 			drawRunnable.get().stop();
 			drawRunnable = Optional.absent();
-		}
-
-	}
-
-	private class DrawSnowflakesRunnable implements Runnable {
-
-		private final int msPerFrame = 1000 / 50; // = 40 FPS
-
-		private final int CHEWING_ANIM_FRAME_LENGTH_IN_MS = 150;
-
-		private final Paint
-				PAINT = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG | Paint.DITHER_FLAG),
-				DEBUG_PAINT = new Paint(),
-				DEBUG_PAINT_STROKE = new Paint();
-		{
-			DEBUG_PAINT.setColor(getResources().getColor(android.R.color.holo_red_light));
-			DEBUG_PAINT_STROKE.setColor(getResources().getColor(android.R.color.holo_red_light));
-			DEBUG_PAINT_STROKE.setStrokeWidth(3);
-			DEBUG_PAINT_STROKE.setStyle(Paint.Style.STROKE);
-		}
-
-		private final SurfaceHolder surfaceHolder;
-		private volatile boolean isRunning = true;
-
-
-		public DrawSnowflakesRunnable(SurfaceHolder surfaceHolder) {
-			this.surfaceHolder = surfaceHolder;
-		}
-
-		@Override
-		public void run() {
-			Canvas tmpCanvas = surfaceHolder.lockCanvas();
-			gameManager.start(tmpCanvas.getWidth(), tmpCanvas.getHeight());
-			surfaceHolder.unlockCanvasAndPost(tmpCanvas);
-
-			while (isRunning) {
-				final Canvas canvas = surfaceHolder.lockCanvas();
-				canvas.drawColor(0, PorterDuff.Mode.CLEAR);
-
-				// draw player
-				Player player = gameManager.getPlayer();
-				switch (player.getState()) {
-					case DEFAULT:
-						canvas.drawBitmap(player.getDefaultBitmap(), player.getMatrix(), PAINT);
-						break;
-
-					case LOOKING_UP:
-						canvas.drawBitmap(player.getLookingUpBitmap(), player.getMatrix(), PAINT);
-						break;
-
-					case CHEWING:
-						if ((player.getChewingDuration() / CHEWING_ANIM_FRAME_LENGTH_IN_MS) % 2 == 0) {
-							canvas.drawBitmap(player.getChewing0Bitmap(), player.getMatrix(), PAINT);
-						} else {
-							canvas.drawBitmap(player.getChewing1Bitmap(), player.getMatrix(), PAINT);
-						}
-						break;
-				}
-
-
-				// draw snowflakes
-				for (Snowflake snowflake : gameManager.getSnowflakes()) {
-					canvas.drawBitmap(snowflake.getBitmap(), snowflake.getMatrix(), PAINT);
-				}
-
-				// draw debug
-				if (DEBUG) {
-					canvas.drawRect(player.getMouthRect(), DEBUG_PAINT_STROKE);
-					canvas.drawCircle(player.getxPos(), player.getyPos(), 3, DEBUG_PAINT);
-					for (Snowflake snowflake : gameManager.getSnowflakes()) {
-						canvas.drawRect(snowflake.getBoundingBox(), DEBUG_PAINT_STROKE);
-						canvas.drawCircle(snowflake.getCenter().x, snowflake.getCenter().y, 3, DEBUG_PAINT);
-					}
-				}
-
-				surfaceHolder.unlockCanvasAndPost(canvas);
-
-				// update game
-				long timeDiff = gameManager.loop();
-
-				try {
-
-					long sleepTime = Math.max(0, msPerFrame + (msPerFrame - timeDiff));
-					Thread.sleep(sleepTime);
-				} catch (InterruptedException e) {
-					Timber.e(e, "interrupted while sleeping in draw");
-				}
-			}
-		}
-
-		public void stop() {
-			isRunning = false;
 		}
 
 	}
