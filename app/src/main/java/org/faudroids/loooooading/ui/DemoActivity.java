5373680d922740e06a64e5726a1a2d10bc8cef6e
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-12-06 17:38:50 +0100

    ui: add variable loading time spinner

--- app/src/main/java/org/faudroids/loooooading/ui/DemoActivity.java
+++ app/src/main/java/org/faudroids/loooooading/ui/DemoActivity.java
@@ -1,6 +1,8 @@
 package org.faudroids.loooooading.ui;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import org.faudroids.loooooading.R;
 
@@ -12,21 +14,31 @@ import roboguice.inject.InjectView;
 public class DemoActivity extends RoboActionBarActivity implements CustomSwipeRefreshLayout.OnRefreshListener {
 
     @InjectView(R.id.layout_swipe) private CustomSwipeRefreshLayout refreshLayout;
+	@InjectView(R.id.spinner_loading_time) private Spinner loadingTimesSpinner;
+	private int[] loadingTimeValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         refreshLayout.setOnRefreshListener(this);
+
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.loading_time_strings, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		loadingTimesSpinner.setAdapter(adapter);
+		loadingTimesSpinner.setSelection(2);
+
+		loadingTimeValues = getResources().getIntArray(R.array.loading_time_values);
     }
 
     @Override
     public void onRefresh() {
+		int loadingTime= loadingTimeValues[loadingTimesSpinner.getSelectedItemPosition()] * 1000;
 		refreshLayout.postDelayed(new Runnable() {
 			@Override
 			public void run() {
 				refreshLayout.refreshComplete();
 			}
-		}, 15000);
+		}, loadingTime);
     }
 }
