01a08167a47dc7180ade41ad93ad798a2bfa1abd
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-12-06 16:59:50 +0100

    ui: merge all activities / layouts into one

--- app/src/main/java/org/faudroids/loooooading/ui/DemoActivity.java
+++ app/src/main/java/org/faudroids/loooooading/ui/DemoActivity.java
@@ -1,8 +1,6 @@
 package org.faudroids.loooooading.ui;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import org.faudroids.loooooading.R;
 
@@ -10,20 +8,16 @@ import roboguice.activity.RoboActionBarActivity;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
 
-@ContentView(R.layout.activity_swipe_refresh)
-public class SwipeRefreshActivity extends RoboActionBarActivity implements CustomSwipeRefreshLayout.OnRefreshListener {
+@ContentView(R.layout.activity_demo)
+public class DemoActivity extends RoboActionBarActivity implements CustomSwipeRefreshLayout.OnRefreshListener {
 
     @InjectView(R.id.layout_swipe) private CustomSwipeRefreshLayout refreshLayout;
-	@InjectView(R.id.list_view) private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         refreshLayout.setOnRefreshListener(this);
-		ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-		listView.setAdapter(listAdapter);
-		listAdapter.addAll("pull", "down", "to", "refresh", "this", "layout", "!", "la", "la", "la", "la", "la", "...");
     }
 
     @Override
@@ -33,6 +27,6 @@ public class SwipeRefreshActivity extends RoboActionBarActivity implements Custo
 			public void run() {
 				refreshLayout.refreshComplete();
 			}
-		}, 2000);
+		}, 10000);
     }
 }
